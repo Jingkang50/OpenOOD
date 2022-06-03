fcf69eba0569d59afc6396857da17e8d9f08cd42
Jingkang Yang <jingkang001@e.ntu.edu.sg>
2022-06-03 14:39:25 +0800

    merge v0.5

--- scripts/ood/conf_branch/train_conf_branch.sh
+++ scripts/ood/conf_branch/train_conf_branch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# sh scripts/e_conf/train_conf_esti.sh
+# sh scripts/ood/train_conf_branch.sh
 
 PYTHONPATH='.':$PYTHONPATH \
-#srun -p dsta --mpi=pmi2 --gres=gpu:${GPU} -n1 \
-#--cpus-per-task=${CPU} --ntasks-per-node=${GPU} \
-#--kill-on-bad-exit=1 --job-name=${jobname} -w SG-IDC1-10-51-2-${node} \
+srun -p dsta --mpi=pmi2 --gres=gpu:${GPU} -n1 \
+--cpus-per-task=${CPU} --ntasks-per-node=${GPU} \
+--kill-on-bad-exit=1 --job-name=${jobname} -w SG-IDC1-10-51-2-${node} \
 python main.py \
 --config configs/datasets/digits/mnist.yml \
 configs/pipelines/train/train_conf_esti.yml \
