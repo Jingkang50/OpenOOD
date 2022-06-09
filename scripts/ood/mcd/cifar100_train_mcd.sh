2dee057f304fab387dabb47981f24ea15ef1ee09
Jingkang Yang <jingkang001@e.ntu.edu.sg>
2022-06-09 16:39:57 +0800

    add config file

--- scripts/ood/mcd/cifar100_train_mcd.sh
+++ scripts/ood/mcd/cifar100_train_mcd.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-# sh scripts/ood/udg/cifar10_train_udg.sh
+# sh scripts/ood/mcd/cifar100_train_mcd.sh
 
 GPU=1
 CPU=1
-node=73
 jobname=openood
 
 PYTHONPATH='.':$PYTHONPATH \
@@ -11,14 +10,14 @@ srun -p dsta --mpi=pmi2 --gres=gpu:${GPU} -n1 \
 --cpus-per-task=${CPU} --ntasks-per-node=${GPU} \
 --kill-on-bad-exit=1 --job-name=${jobname} \
 python -m pdb -c continue main.py \
---config configs/datasets/cifar10/cifar10.yml \
-configs/datasets/cifar10/cifar10_oe.yml \
-configs/networks/udg_net.yml \
+--config configs/datasets/cifar100/cifar100.yml \
+configs/datasets/cifar100/cifar100_oe.yml \
+configs/networks/mcd_net.yml \
 configs/preprocessors/base_preprocessor.yml \
 configs/pipelines/train/baseline.yml \
-configs/pipelines/train/train_udg.yml \
---dataset.train.dataset_class UDGDataset \
---dataset.oe.dataset_class UDGDataset \
+configs/pipelines/train/train_mcd.yml \
 --network.backbone.name resnet18_32x32 \
 --network.pretrained False \
+--dataset.image_size 32 \
+--optimizer.num_epochs 100 \
 --num_workers 4
--- scripts/ood/oe/cifar100_train_oe.sh
+++ scripts/ood/oe/cifar100_train_oe.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# sh scripts/ood/udg/cifar10_train_udg.sh
+# sh scripts/ood/oe/cifar100_train_oe.sh
 
 GPU=1
 CPU=1
@@ -10,15 +10,16 @@ PYTHONPATH='.':$PYTHONPATH \
 srun -p dsta --mpi=pmi2 --gres=gpu:${GPU} -n1 \
 --cpus-per-task=${CPU} --ntasks-per-node=${GPU} \
 --kill-on-bad-exit=1 --job-name=${jobname} \
+-w SG-IDC1-10-51-2-${node} \
 python -m pdb -c continue main.py \
---config configs/datasets/cifar10/cifar10.yml \
-configs/datasets/cifar10/cifar10_oe.yml \
-configs/networks/udg_net.yml \
+--config configs/datasets/cifar100/cifar100.yml \
+configs/datasets/cifar100/cifar100_oe.yml \
+configs/networks/resnet18_32x32.yml \
 configs/preprocessors/base_preprocessor.yml \
 configs/pipelines/train/baseline.yml \
-configs/pipelines/train/train_udg.yml \
---dataset.train.dataset_class UDGDataset \
---dataset.oe.dataset_class UDGDataset \
---network.backbone.name resnet18_32x32 \
+configs/pipelines/train/train_oe.yml \
 --network.pretrained False \
---num_workers 4
+--dataset.image_size 32 \
+--optimizer.num_epochs 100 \
+--num_workers 4 \
+--mark 0
