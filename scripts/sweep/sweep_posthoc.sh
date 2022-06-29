0e34ceac5204a806c631fd32fa68de76f2cd8c8b
JediWarriorZou <dejianzou1206@gmail.com>
2022-06-29 16:54:05 +0800

    osr

--- scripts/sweep/sweep_posthoc.sh
+++ scripts/sweep/sweep_posthoc.sh
@@ -1,7 +1,7 @@
 # sh ./scripts/sweep/sweep_posthoc.sh
 python ./scripts/sweep/sweep_posthoc.py \
 --benchmarks 'mnist' \
---methods 'odin' \
+--methods 'msp' \
 --metrics 'ood' \
 --output-dir './results/ood' \
---launcher 'slurm' \
+--launcher 'local' \
