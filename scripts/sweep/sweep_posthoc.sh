282e67edc7de57c9f4d64ab20dc374617a9f1e03
Jingkang Yang <jingkang001@e.ntu.edu.sg>
2022-06-29 16:38:33 +0800

    modify config

--- scripts/sweep/sweep_posthoc.sh
+++ scripts/sweep/sweep_posthoc.sh
@@ -1,7 +1,8 @@
 # sh ./scripts/sweep/sweep_posthoc.sh
 python ./scripts/sweep/sweep_posthoc.py \
---benchmarks 'mnist' \
---methods 'odin' \
+--benchmarks 'cifar6' \
+--methods 'msp' \
 --metrics 'ood' \
+--metric2save 'auroc' \
 --output-dir './results/ood' \
 --launcher 'slurm' \
