68caceffa37b4cbf4b22bb4caeb145a9877a419a
Jingkang Yang <jingkang001@e.ntu.edu.sg>
2022-06-30 11:42:55 +0800

    fix tin20

--- scripts/basics/osr_mnist6/train_mnist6.sh
+++ scripts/basics/osr_mnist6/train_mnist6.sh
@@ -12,15 +12,11 @@ if [ $USER == "jkyang" ]; then
     --cpus-per-task=${CPU} --ntasks-per-node=${GPU} \
     --kill-on-bad-exit=1 --job-name=${jobname} \
     python main.py \
-    --config configs/datasets/osr_mnist6/mnist6_seed5.yml \
+    --config configs/datasets/osr_mnist6/mnist6_seed2.yml \
     configs/networks/lenet.yml \
     configs/preprocessors/base_preprocessor.yml \
     configs/pipelines/train/baseline.yml \
-    --network.pretrained False \
-    --dataset.image_size 28 \
-    --optimizer.num_epochs 100 \
-    --num_workers 4 \
-    --mark seed5 &
+    --network.pretrained False
 else
     PYTHONPATH='.':$PYTHONPATH \
     python main.py \
@@ -33,3 +29,5 @@ else
     --optimizer.num_epochs 100 \
     --num_workers 4
 fi
+
+cp ./results/cifar6_seed5_resnet18_32x32_base_e100_lr0.1_default/best.ckpt ./results/checkpoints/osr/cifar6_seed5.ckpt
