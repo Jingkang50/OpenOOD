286866aabf9d94299c4024f3317ea1adea90bf5c
Jingyang <jingyang.zhang@duke.edu>
2023-03-27 18:21:15 -0400

    fix pixmix bugs and enable changing IMAGE_SIZE

--- openood/preprocessors/pixmix_preprocessor.py
+++ openood/preprocessors/pixmix_preprocessor.py
@@ -20,34 +20,34 @@ resize_list = {
 
 
 class PixMixPreprocessor(BasePreprocessor):
-    def __init__(self, config, split):
-        self.config = config
-        dataset_name = config.dataset.name.split('_')[0]
-        image_size = config.dataset.image_size
-        self.args = self.config.preprocessor.preprocessor_args
-        self.tensorize = tvs_trans.ToTensor()
-        if dataset_name in normalization_dict.keys():
-            mean = normalization_dict[dataset_name][0]
-            std = normalization_dict[dataset_name][1]
+    def __init__(self, config):
+        self.pre_size = config.dataset.pre_size
+        self.dataset_name = config.dataset.name.split('_')[0]
+        self.image_size = config.dataset.image_size
+        self.interpolation = interpolation_modes[config.dataset.interpolation]
+        normalization_type = config.dataset.normalization_type
+        if normalization_type in normalization_dict.keys():
+            self.mean = normalization_dict[normalization_type][0]
+            self.std = normalization_dict[normalization_type][1]
         else:
-            mean = [0.5, 0.5, 0.5]
-            std = [0.5, 0.5, 0.5]
-        self.normalize = tvs_trans.Normalize(mean, std)  # ? use which one ?
-        pre_size = config.dataset.pre_size
-        interpolation = interpolation_modes[
-            config.dataset['train'].interpolation]
+            self.mean = [0.5, 0.5, 0.5]
+            self.std = [0.5, 0.5, 0.5]
+        self.normalize = tvs_trans.Normalize(self.mean, self.std)
+        self.tensorize = tvs_trans.ToTensor()
+
+        self.args = config.preprocessor.preprocessor_args
 
         self.transform = tvs_trans.Compose([
             Convert('RGB'),
-            tvs_trans.Resize(pre_size, interpolation=interpolation),
-            tvs_trans.CenterCrop(image_size),
+            tvs_trans.Resize(self.pre_size, interpolation=self.interpolation),
+            tvs_trans.CenterCrop(self.image_size),
             tvs_trans.RandomHorizontalFlip(),
-            tvs_trans.RandomCrop(image_size, padding=4),
+            tvs_trans.RandomCrop(self.image_size, padding=4),
         ])
 
         self.mixing_set_transform = tvs_trans.Compose([
-            tvs_trans.Resize(resize_list[dataset_name]),
-            tvs_trans.RandomCrop(image_size)
+            tvs_trans.Resize(resize_list[self.dataset_name]),
+            tvs_trans.RandomCrop(self.image_size)
         ])
 
         with open(self.args.mixing_set_dir, 'r') as f:
@@ -66,30 +66,29 @@ class PixMixPreprocessor(BasePreprocessor):
     def augment_input(self, image):
         aug_list = augmentations_all if self.args.all_ops else augmentations
         op = np.random.choice(aug_list)
-        return op(image.copy(), self.args.aug_severity)
+        return op(image.copy(), self.args.aug_severity, self.image_size)
 
     def pixmix(self, orig, mixing_pic):
         mixings = [add, multiply]
-        orig = self.transform(orig)  
-       
+        orig = self.transform(orig)
+
         # do basic augmentation first
         mixing_pic = self.mixing_set_transform(mixing_pic)
-    
+
         if np.random.random() < 0.5:
             mixed = self.tensorize(self.augment_input(orig))
         else:
             mixed = self.tensorize(orig)
 
-        
         for _ in range(np.random.randint(self.args.k + 1)):
 
             if np.random.random() < 0.5:
                 aug_image_copy = self.tensorize(self.augment_input(orig))
             else:
                 aug_image_copy = self.tensorize(mixing_pic)
-         
+
             mixed_op = np.random.choice(mixings)
-            
+
             mixed = mixed_op(mixed, aug_image_copy, self.args.beta)
             mixed = torch.clip(mixed, 0, 1)
 
@@ -97,9 +96,6 @@ class PixMixPreprocessor(BasePreprocessor):
 
 
 """Base augmentations operators."""
-
-# ImageNet code should change this value
-IMAGE_SIZE = 28
 
 #########################################################
 #################### AUGMENTATIONS ######################
@@ -136,32 +132,32 @@ def sample_level(n):
     return np.random.uniform(low=0.1, high=n)
 
 
-def autocontrast(pil_img, _):
+def autocontrast(pil_img, _, IMAGE_SIZE):
     return ImageOps.autocontrast(pil_img)
 
 
-def equalize(pil_img, _):
+def equalize(pil_img, _, IMAGE_SIZE):
     return ImageOps.equalize(pil_img)
 
 
-def posterize(pil_img, level):
+def posterize(pil_img, level, IMAGE_SIZE):
     level = int_parameter(sample_level(level), 4)
     return ImageOps.posterize(pil_img, 4 - level)
 
 
-def rotate(pil_img, level):
+def rotate(pil_img, level, IMAGE_SIZE):
     degrees = int_parameter(sample_level(level), 30)
     if np.random.uniform() > 0.5:
         degrees = -degrees
     return pil_img.rotate(degrees, resample=Image.BILINEAR)
 
 
-def solarize(pil_img, level):
+def solarize(pil_img, level, IMAGE_SIZE):
     level = int_parameter(sample_level(level), 256)
     return ImageOps.solarize(pil_img, 256 - level)
 
 
-def shear_x(pil_img, level):
+def shear_x(pil_img, level, IMAGE_SIZE):
     level = float_parameter(sample_level(level), 0.3)
     if np.random.uniform() > 0.5:
         level = -level
@@ -170,7 +166,7 @@ def shear_x(pil_img, level):
                              resample=Image.BILINEAR)
 
 
-def shear_y(pil_img, level):
+def shear_y(pil_img, level, IMAGE_SIZE):
     level = float_parameter(sample_level(level), 0.3)
     if np.random.uniform() > 0.5:
         level = -level
@@ -179,7 +175,7 @@ def shear_y(pil_img, level):
                              resample=Image.BILINEAR)
 
 
-def translate_x(pil_img, level):
+def translate_x(pil_img, level, IMAGE_SIZE):
     level = int_parameter(sample_level(level), IMAGE_SIZE / 3)
     if np.random.random() > 0.5:
         level = -level
@@ -188,7 +184,7 @@ def translate_x(pil_img, level):
                              resample=Image.BILINEAR)
 
 
-def translate_y(pil_img, level):
+def translate_y(pil_img, level, IMAGE_SIZE):
     level = int_parameter(sample_level(level), IMAGE_SIZE / 3)
     if np.random.random() > 0.5:
         level = -level
@@ -198,25 +194,25 @@ def translate_y(pil_img, level):
 
 
 # operation that overlaps with ImageNet-C's test set
-def color(pil_img, level):
+def color(pil_img, level, IMAGE_SIZE):
     level = float_parameter(sample_level(level), 1.8) + 0.1
     return ImageEnhance.Color(pil_img).enhance(level)
 
 
 # operation that overlaps with ImageNet-C's test set
-def contrast(pil_img, level):
+def contrast(pil_img, level, IMAGE_SIZE):
     level = float_parameter(sample_level(level), 1.8) + 0.1
     return ImageEnhance.Contrast(pil_img).enhance(level)
 
 
 # operation that overlaps with ImageNet-C's test set
-def brightness(pil_img, level):
+def brightness(pil_img, level, IMAGE_SIZE):
     level = float_parameter(sample_level(level), 1.8) + 0.1
     return ImageEnhance.Brightness(pil_img).enhance(level)
 
 
 # operation that overlaps with ImageNet-C's test set
-def sharpness(pil_img, level):
+def sharpness(pil_img, level, IMAGE_SIZE):
     level = float_parameter(sample_level(level), 1.8) + 0.1
     return ImageEnhance.Sharpness(pil_img).enhance(level)
 
