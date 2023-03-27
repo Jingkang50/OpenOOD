use more robust kl div; fix corner cases for klm

openood/postprocessors/kl_matching_postprocessor.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from sklearn.metrics import pairwise_distances_argmin_min
+import scipy
 from tqdm import tqdm
 
 from .base_postprocessor import BasePostprocessor
@@ -13,13 +14,10 @@ from .base_postprocessor import BasePostprocessor
 class KLMatchingPostprocessor(BasePostprocessor):
     def __init__(self, config):
         super().__init__(config)
-        self.args = self.config.postprocessor.postprocessor_args
-        self.dim = self.args.dim
         self.num_classes = self.config.dataset.num_classes
-        self.args_dict = self.config.postprocessor.postprocessor_sweep
 
     def kl(self, p, q):
-        return np.sum(np.where(p != 0, p * np.log(p / q), 0))
+        return scipy.stats.entropy(p, q)
 
     def setup(self, net: nn.Module, id_loader_dict, ood_loader_dict):
         net.eval()
@@ -39,10 +37,18 @@ class KLMatchingPostprocessor(BasePostprocessor):
 
         all_softmax = torch.cat(all_softmax)
         preds = torch.cat(preds)
-        self.mean_softmax_val = [
-            all_softmax[preds.eq(i)].mean(0).numpy()
-            for i in tqdm(range(self.num_classes))
-        ]
+
+        self.mean_softmax_val = []
+        for i in tqdm(range(self.num_classes)):
+            # if there are no validation samples
+            # for this category
+            if torch.sum(preds.eq(i).float()) == 0:
+                temp = np.zeros((self.num_classes, ))
+                temp[i] = 1
+                self.mean_softmax_val.append(temp)
+            else:
+                self.mean_softmax_val.append(
+                    all_softmax[preds.eq(i)].mean(0).numpy())
 
     @torch.no_grad()
     def postprocess(self, net: nn.Module, data: Any):
@@ -52,9 +58,3 @@ class KLMatchingPostprocessor(BasePostprocessor):
         scores = -pairwise_distances_argmin_min(
             softmax, np.array(self.mean_softmax_val), metric=self.kl)[1]
         return preds, torch.from_numpy(scores)
-
-    def set_hyperparam(self, hyperparam: list):
-        self.dim = hyperparam[0]
-
-    def get_hyperparam(self):
-        return self.dim