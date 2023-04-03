d6c0259a2005305f1a96b1069f464ec8bffd370a
Jingyang <jingyang.zhang@duke.edu>
2023-04-02 21:49:19 -0400

    remove redundant code

--- openood/postprocessors/vim_postprocessor.py
+++ openood/postprocessors/vim_postprocessor.py
@@ -37,28 +37,12 @@ class VIMPostprocessor(BasePostprocessor):
                 feature_id_train = np.concatenate(feature_id_train, axis=0)
                 logit_id_train = feature_id_train @ self.w.T + self.b
 
-                print('Extracting id validation feature')
-                feature_id_val = []
-                for batch in tqdm(id_loader_dict['val'],
-                                  desc='Eval: ',
-                                  position=0,
-                                  leave=True):
-                    data = batch['data'].cuda()
-                    data = data.float()
-                    _, feature = net(data, return_feature=True)
-                    feature_id_val.append(feature.cpu().numpy())
-                feature_id_val = np.concatenate(feature_id_val, axis=0)
-                logit_id_val = feature_id_val @ self.w.T + self.b
-
             self.u = -np.matmul(pinv(self.w), self.b)
             ec = EmpiricalCovariance(assume_centered=True)
             ec.fit(feature_id_train - self.u)
             eig_vals, eigen_vectors = np.linalg.eig(ec.covariance_)
             self.NS = np.ascontiguousarray(
                 (eigen_vectors.T[np.argsort(eig_vals * -1)[self.dim:]]).T)
-
-            # import pdb
-            # pdb.set_trace()
 
             vlogit_id_train = norm(np.matmul(feature_id_train - self.u,
                                              self.NS),
@@ -67,10 +51,6 @@ class VIMPostprocessor(BasePostprocessor):
                 axis=-1).mean() / vlogit_id_train.mean()
             print(f'{self.alpha=:.4f}')
 
-            vlogit_id_val = norm(np.matmul(feature_id_val - self.u, self.NS),
-                                 axis=-1) * self.alpha
-            energy_id_val = logsumexp(logit_id_val, axis=-1)
-            self.score_id = -vlogit_id_val + energy_id_val
             self.setup_flag = True
         else:
             pass
