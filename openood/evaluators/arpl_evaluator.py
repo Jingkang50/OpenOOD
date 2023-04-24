4abcc2c07f5f270d3f980d6eb977af09e1e3842e
Jingyang <jingyang.zhang@duke.edu>
2023-04-24 16:23:36 -0400

    several fixes to get fsood eval working

--- openood/evaluators/arpl_evaluator.py
+++ openood/evaluators/arpl_evaluator.py
@@ -1,10 +1,8 @@
-import os
-from typing import Dict, List
+from typing import Dict
 
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
@@ -22,7 +20,9 @@ class ARPLEvaluator(OODEvaluator):
                  net: dict,
                  data_loader: DataLoader,
                  postprocessor: BasePostprocessor = None,
-                 epoch_idx: int = -1):
+                 epoch_idx: int = -1,
+                 fsood: bool = False,
+                 csid_data_loaders: Dict[str, DataLoader] = None):
         criterion = net['criterion']
         net = net['netF']
         net.eval()
@@ -49,15 +49,52 @@ class ARPLEvaluator(OODEvaluator):
                 # test loss average
                 loss_avg += float(loss.data)
 
-        metrics = {}
-        metrics['epoch_idx'] = epoch_idx
-        metrics['loss'] = loss_avg / len(data_loader)
-        metrics['acc'] = correct / len(data_loader.dataset)
-        return metrics
+        if not fsood:
+            metrics = {}
+            metrics['epoch_idx'] = epoch_idx
+            metrics['loss'] = loss_avg / len(data_loader)
+            metrics['acc'] = correct / len(data_loader.dataset)
+            return metrics
+        else:
+            all_correct = 0
+            all_total = 0
+            all_correct += correct
+            all_total += len(data_loader.dataset)
 
-    def eval_ood(self, net: dict, id_data_loader: DataLoader,
+            assert csid_data_loaders is not None
+            for dataset_name, csid_dl in csid_data_loaders.items():
+                correct = 0
+                with torch.no_grad():
+                    for batch in tqdm(csid_dl,
+                                      desc='Eval: ',
+                                      position=0,
+                                      leave=True):
+                        # prepare data
+                        data = batch['data'].cuda()
+                        target = batch['label'].cuda()
+
+                        # forward
+                        _, feat = net(data, return_feature=True)
+                        output, loss = criterion(feat, target)
+
+                        # accuracy
+                        pred = output.data.max(1)[1]
+                        correct += pred.eq(target.data).sum().item()
+
+                all_correct += correct
+                all_total += len(csid_dl.dataset)
+
+            metrics = {}
+            metrics['epoch_idx'] = epoch_idx
+            metrics['acc'] = all_correct / all_total
+            return metrics
+
+    def eval_ood(self,
+                 net: dict,
+                 id_data_loader: DataLoader,
                  ood_data_loaders: Dict[str, Dict[str, DataLoader]],
-                 postprocessor: BasePostprocessor):
+                 postprocessor: BasePostprocessor,
+                 fsood: bool = False):
         criterion = net['criterion']
         net = net['netF']
         net = nn.Sequential(
@@ -74,11 +111,27 @@ class ARPLEvaluator(OODEvaluator):
             net, id_data_loader['test'])
         if self.config.recorder.save_scores:
             self._save_scores(id_pred, id_conf, id_gt, dataset_name)
+
+        if fsood:
+            # load csid data and compute confidence
+            for dataset_name, csid_dl in ood_data_loaders['csid'].items():
+                print(f'Performing inference on {dataset_name} dataset...',
+                      flush=True)
+                csid_pred, csid_conf, csid_gt = postprocessor.inference(
+                    net, csid_dl)
+                if self.config.recorder.save_scores:
+                    self._save_scores(csid_pred, csid_conf, csid_gt,
+                                      dataset_name)
+                id_pred = np.concatenate([id_pred, csid_pred])
+                id_conf = np.concatenate([id_conf, csid_conf])
+                id_gt = np.concatenate([id_gt, csid_gt])
+
         # load nearood data and compute ood metrics
         self._eval_ood(net, [id_pred, id_conf, id_gt],
                        ood_data_loaders,
                        postprocessor,
                        ood_split='nearood')
+
         # load farood data and compute ood metrics
         self._eval_ood(net, [id_pred, id_conf, id_gt],
                        ood_data_loaders,
