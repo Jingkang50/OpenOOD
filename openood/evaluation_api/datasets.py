fd879e7eeb21e79858039c406d603e617a9b0545
Jingyang <jingyang.zhang@duke.edu>
2023-06-16 23:03:54 -0400

    update benchmark_imglist and remove tutorial ipynb

--- openood/evaluation_api/datasets.py
+++ openood/evaluation_api/datasets.py
@@ -321,7 +321,7 @@ download_id_dict = {
     'imagenet_v2': '1akg2IiE22HcbvTBpwXQoD7tgfPCdkoho',
     'imagenet_r': '1EzjMN2gq-bVV7lg-MEAdeuBuz-7jbGYU',
     'imagenet_c': '1JeXL9YH4BO8gCJ631c5BHbaSsl-lekHt',
-    'benchmark_imglist': '1VDorb6Xe1VhlAxbXwp-kjd-caUddKd6x'
+    'benchmark_imglist': '1XKzBdWCqg3vPoj-D32YixJyJJ0hL63gP'
 }
 
 dir_dict = {
