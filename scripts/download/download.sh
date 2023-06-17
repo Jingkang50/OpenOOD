6914b75a5cf8207e018730cdc30e628f44782634
Jingyang <jingyang.zhang@duke.edu>
2023-06-17 00:24:34 -0400

    update download script

--- scripts/download/download.sh
+++ scripts/download/download.sh
@@ -1,7 +1,10 @@
 # sh ./scripts/download/dowanload.sh
-python ./scripts/download/dowanload.py \
---contents 'datasets' 'checkpoints' \
---datasets 'default' \
---checkpoints 'all' \
---save_dir './data' './results' \
---dataset_mode 'benchmark'
+
+# download the up-to-date benchmarks and checkpoints
+# provided by OpenOOD v1.5
+python ./scripts/download/download.py \
+	--contents 'datasets' 'checkpoints' \
+	--datasets 'ood_v1.5' \
+	--checkpoints 'ood_v1.5' \
+	--save_dir './data' './results' \
+	--dataset_mode 'benchmark'
